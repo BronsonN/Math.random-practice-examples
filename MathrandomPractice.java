f6f6e03f684c38312af4692956bf6bfb39675d24
Bronson <58447783+BronsonN@users.noreply.github.com>
2020-10-04 13:18:34 -0600

    Add files via upload

---
+++ MathrandomPractice.java
@@ -0,0 +1,18 @@
+
+
+public class MathrandomPractice {
+
+  public static void main(String[] args) {
+
+
+    //How do you generate a random integer i such that 0 <= i < 20 ?
+    int number = (int)(Math.random() * 20);
+
+    //How do you generate a random integer i such that 10 <= i < 20
+    //int number = 10 + (int)(Math.random() * 10);
+
+    System.out.println(number);
+
+  }
+
+}
